c750ef051f29ca33839bc65321953510d208fada
qdorme <qdorme@test.fr>
2018-03-17 21:10:13 +0100

    creation of executable jar and externalisation of timer

--- src/main/kotlin/fr/qdorme/Run.kt
+++ src/main/kotlin/fr/qdorme/Run.kt
@@ -4,14 +4,13 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-const val sleep = 25L
-
 fun main(args: Array<String>) {
 
     var mask: BufferedImage?
     var cols:Int
     var rows:Int
     var maze : Maze
+    var sleep = 15L
     when (args.size){
         1 ->  {
             mask = ImageIO.read(File(args[0]))
@@ -20,8 +19,22 @@ fun main(args: Array<String>) {
             maze = Maze(cols, rows, mask, sleep)
         }
         2 -> {
+            try {
+                cols = args[0].toInt() + 2
+                rows = args[1].toInt() + 2
+                maze = Maze(cols, rows,null, sleep)
+            }catch (e:NumberFormatException){
+                mask = ImageIO.read(File(args[0]))
+                cols = mask.data.width
+                rows = mask.data.height
+                sleep = args[1].toLong()
+                maze = Maze(cols, rows, mask, sleep)
+            }
+        }
+        3 -> {
             cols = args[0].toInt() + 2
             rows = args[1].toInt() + 2
+            sleep = args[2].toLong()
             maze = Maze(cols, rows,null, sleep)
         }
         else -> {
